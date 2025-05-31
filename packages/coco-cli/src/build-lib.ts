192b71dc4de0858f6500b62ee3b2fd270cf703bc
jcg <jiang.chenguang@outlook.com>
2025-05-31 17:31:21 +0800

    feat: rollup配置挪到build-lib内部

--- packages/coco-cli/src/build-lib.ts
+++ packages/coco-cli/src/build-lib.ts
@@ -1,19 +1,57 @@
 import path from 'node:path';
-import fs from 'node:fs';
-import rollup from 'rollup';
-import { execSync, spawn } from 'child_process';
+import { rollup } from 'rollup';
 import process from 'node:process';
-import { resolveCli } from './util/resolve';
+import typescript from '@rollup/plugin-typescript';
+import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import tailwindcss from 'tailwindcss';
 
 export const build = async () => {
-  const rollup = resolveCli('rollup');
-  const configPath = path.resolve(
-    __dirname,
-    '../build-config/lib/rollup.config.js'
-  );
-  const args = ['--config', configPath];
-  return spawn(rollup, args, {
-    cwd: process.cwd(),
-    stdio: 'inherit',
+  const result = await rollup({
+    input: path.join(process.cwd(), './src/lib.ts'),
+    plugins: [
+      typescript({
+        compilerOptions: {
+          target: 'ESNext',
+          lib: ['dom', 'esnext'],
+          declaration: true,
+          declarationDir: './dist/types',
+          jsx: 'preserve',
+          resolveJsonModule: true,
+          plugins: [
+            {
+              transform: '@cocojs/typescript-transformer',
+              transformProgram: true,
+            },
+          ],
+        },
+      }),
+      babel({
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        plugins: [
+          [
+            require.resolve('@babel/plugin-proposal-decorators'),
+            { version: '2023-11' },
+          ],
+          [
+            require.resolve('@babel/plugin-transform-react-jsx', {
+              paths: [path.resolve(__dirname, '..', '../node_modules')],
+            }),
+            {
+              runtime: 'automatic',
+              importSource: 'coco-mvc',
+            },
+          ],
+        ],
+      }),
+      postcss({
+        plugins: [tailwindcss()],
+        extract: true,
+      }),
+    ],
+  });
+  await result.write({
+    file: path.join(process.cwd(), './dist/bundle.esm.js'),
+    format: 'es',
   });
 };
