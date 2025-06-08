338d266291bc972bd131e9ee21561c32c7af9e5b
jcg <jiang.chenguang@outlook.com>
2025-06-08 11:39:09 +0800

    feat: 添加测试用例

--- packages/coconut-web/src/__tests__/ReactDOM-test.jsx
+++ packages/coconut-web/src/__tests__/ReactDOM-test.jsx
@@ -1,5 +1,14 @@
+// packages/react-dom/src/__tests__/ReactDOM-test.js
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails react-core
+ */
 import { render, registerApplication, unregisterApplication } from '../index';
-import { Application, component } from 'coco-mvc'
+import { Application, component, view } from 'coco-mvc'
 import { getByRole, getByText, getRoles, waitFor } from '@testing-library/dom';
 import * as ReactTestUtils from './test-units/ReactTestUnits';
 
@@ -14,14 +23,7 @@ describe('ReactDOM', () => {
     unregisterApplication();
   })
 
-  test('allows a DOM element to be used with a string', () => {
-    application.start();
-    const element = <div className={'foo'} />;
-    const node = ReactTestUtils.renderIntoDocument(element);
-    expect(node.tagName).toBe('DIV');
-  });
-
-  test('should bubble onSubmit', async () => {
+  it('should bubble onSubmit', async () => {
     const container = document.createElement('p');
     let count = 0;
     let buttonRef;
@@ -65,6 +67,133 @@ describe('ReactDOM', () => {
       await waitFor(async () => {
         expect(count).toBe(1);
       });
+    } finally {
+      document.body.removeChild(container);
+    }
+  });
+
+  it('allows a DOM element to be used with a string', () => {
+    application.start();
+    const element = <div className={'foo'} />;
+    const node = ReactTestUtils.renderIntoDocument(element);
+    expect(node.tagName).toBe('DIV');
+  });
+
+  it('should allow children to be passed as an argument', () => {
+    const argNode = ReactTestUtils.renderIntoDocument(<div>child</div>);
+    expect(argNode.innerHTML).toBe('child');
+  });
+
+  it('should overwrite props.children with children argument', () => {
+    const conflictNode = ReactTestUtils.renderIntoDocument(
+      <div children='fakechild'>child</div>
+    );
+    expect(conflictNode.innerHTML).toBe('child');
+  });
+
+  /**
+   * We need to make sure that updates occur to the actual node that's in the
+   * DOM, instead of a stale cache.
+   */
+  it('should purge the DOM cache when removing nodes', () => {
+    let myDiv = ReactTestUtils.renderIntoDocument(
+      <div>
+        <div key="theDog" className="dog" />,
+        <div key="theBird" className="bird" />
+      </div>,
+    );
+    // Warm the cache with theDog
+    myDiv = ReactTestUtils.renderIntoDocument(
+      <div>
+        <div key="theDog" className="dogbeforedelete" />,
+        <div key="theBird" className="bird" />,
+      </div>,
+    );
+    // Remove theDog - this should purge the cache
+    myDiv = ReactTestUtils.renderIntoDocument(
+      <div>
+        <div key="theBird" className="bird" />,
+      </div>,
+    );
+    // Now, put theDog back. It's now a different DOM node.
+    myDiv = ReactTestUtils.renderIntoDocument(
+      <div>
+        <div key="theDog" className="dog" />,
+        <div key="theBird" className="bird" />,
+      </div>,
+    );
+    // Change the className of theDog. It will use the same element
+    myDiv = ReactTestUtils.renderIntoDocument(
+      <div>
+        <div key="theDog" className="bigdog" />,
+        <div key="theBird" className="bird" />,
+      </div>,
+    );
+    const dog = myDiv.childNodes[0];
+    expect(dog.className).toBe('bigdog');
+  });
+
+  it('preserves focus', () => {
+    let input;
+    let input2;
+
+    @component()
+    class A {
+      props
+      render() {
+        return (
+          <div>
+            <input id="one" ref={r => (input = input || r)} />
+            {this.props.showTwo && (
+              <input id="two" ref={r => (input2 = input2 || r)} />
+            )}
+          </div>
+        );
+      }
+
+      viewDidUpdate() {
+        // Focus should have been restored to the original input
+        expect(document.activeElement.id).toBe('one');
+        input2.focus();
+        expect(document.activeElement.id).toBe('two');
+        log.push('input2 focused');
+      }
+    }
+
+    application.start();
+    const log = [];
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    try {
+      render(<A showTwo={false} />, container);
+      input.focus();
+
+      // When the second input is added, let's simulate losing focus, which is
+      // something that could happen when manipulating DOM nodes (but is hard to
+      // deterministically force without relying intensely on React DOM
+      // implementation details)
+      const div = container.firstChild;
+      ['appendChild', 'insertBefore'].forEach(name => {
+        const mutator = div[name];
+        div[name] = function() {
+          if (input) {
+            input.blur();
+            expect(document.activeElement.tagName).toBe('BODY');
+            log.push('input2 inserted');
+          }
+          return mutator.apply(this, arguments);
+        };
+      });
+
+      expect(document.activeElement.id).toBe('one');
+      render(<A showTwo={true} />, container);
+      // input2 gets added, which causes input to get blurred. Then
+      // componentDidUpdate focuses input2 and that should make it down to here,
+      // not get overwritten by focus restoration.
+      waitFor(() => {
+        expect(document.activeElement.id).toBe('two');
+        expect(log).toEqual(['input2 inserted', 'input2 focused']);
+      })
     } finally {
       document.body.removeChild(container);
     }
