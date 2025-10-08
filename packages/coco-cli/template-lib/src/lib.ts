d4f2467ee990f2c97f5ba0f92e7138267a955416
jcg <jiang.chenguang@outlook.com>
2025-10-08 10:28:35 +0800

    feat: 更新库的模版

--- packages/coco-cli/template-lib/src/lib.ts
+++ packages/coco-cli/template-lib/src/lib.ts
@@ -1,4 +1,3 @@
-import './input.css';
 export { default as Link } from './component/link';
 export { default as log } from './decorator/log';
 export { default as Log } from './decorator/metadata/log';
---
+++ packages/coco-cli/template-lib/src/page/link-page.tsx
@@ -0,0 +1,16 @@
+import { page, route } from 'coco-mvc';
+import Link from '../view/link';
+
+@route('/link')
+@page()
+class LinkPage {
+  render() {
+    return (
+      <div>
+        <Link />
+      </div>
+    );
+  }
+}
+
+export default LinkPage;
