8fa96a9bf268748e2e77e48e37303ad0d31b1a81
jcg <jiang.chenguang@outlook.com>
2025-12-15 20:49:43 +0800

    feat: fix 打印

--- packages/coco-cli/src/webpack-process/get-webpack-config.ts
+++ packages/coco-cli/src/webpack-process/get-webpack-config.ts
@@ -40,17 +40,9 @@ const buildInConfig = {
                         },
                     },
                     function debugAfterTsLoader(source: string) {
-                        const resourcePath = this.resourcePath;
-
-                        // 只打印你关心的文件，避免刷屏（可选）
-                        if (resourcePath.includes('src/index')) {
-                            console.log('======== debug-after-ts-loader ========');
-                            console.log('FILE:', resourcePath);
-                            console.log('-------- SOURCE START --------');
-                            console.log(source);
-                            console.log('-------- SOURCE END ----------');
-                        }
-
+                        console.log('-------- SOURCE START --------');
+                        console.log(source);
+                        console.log('-------- SOURCE END ----------');
                         return source;
                     },
                     {
